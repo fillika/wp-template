27dfeba57cc4f8c6f08ae4447af03f6862c4a807
fillika <49833052+fillika@users.noreply.github.com>
2019-11-02 23:29:19 +0300

    Установил минификатор css и js, но теперь исчезла стандартная source map. Попробовать пофиксить

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 /* Modules */
 const css = {
@@ -51,6 +53,12 @@ const config = {
     path: path.resolve(__dirname, "./dist")
   },
   devServer: devServer,
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin({}),
+      new UglifyJsPlugin({ test: /\.js(\?.*)?$/i })
+    ]
+  },
   plugins: plugins,
   module: modules
 };
