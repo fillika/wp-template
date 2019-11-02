343e7b8d75295b460effd44fa75d86e09240f728
fillika <49833052+fillika@users.noreply.github.com>
2019-11-02 17:16:21 +0300

    Добавил html плагин для работы с html

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
   entry: "./src/js/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist/js")
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    })
+  ]
 };
 
 module.exports = config;
